ea4e7260d6696d1d7dafa55e2da1009959bca7ef
Gary <cnahzjl@gmail.com>
2024-04-25 22:24:39 +0800

    完善灵签接口

--- internal/server/http.go
+++ internal/server/http.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"embed"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	swaggerfiles "github.com/swaggo/files"
@@ -16,6 +17,9 @@ import (
 	"strings"
 )
 
+//go:embed templates
+var f embed.FS
+
 func NewHTTPServer(
 	logger *log.Logger,
 	conf *viper.Viper,
@@ -26,31 +30,35 @@ func NewHTTPServer(
 	signInHandler *handler.SignInHandler,
 	spiritSignHandler *handler.SpiritSignHandler,
 ) *http.Server {
-	gin.SetMode(gin.DebugMode)
+	if conf.GetString("env") == "local" {
+		gin.SetMode(gin.DebugMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
 	s := http.NewServer(
 		gin.Default(),
 		logger,
 		http.WithServerHost(conf.GetString("http.host")),
 		http.WithServerPort(conf.GetInt("http.port")),
 	)
-	// 定义自定义函数
-	funcMap := template.FuncMap{
+	s.Static("/css", "./web/static/css")
+	// 加载模板引擎，并指定自定义函数
+	tmpl := template.Must(template.New("").Funcs(template.FuncMap{
 		"replace": func(s, old, new string) template.HTML {
 			return template.HTML(strings.ReplaceAll(s, old, new))
 		},
 		"split": strings.Split,
+	}).ParseFS(f, "templates/**/*.html"))
+	s.SetHTMLTemplate(tmpl)
+	if conf.GetString("env") == "local" {
+		// swagger doc
+		docs.SwaggerInfo.BasePath = "/v1"
+		s.GET("/swagger/*any", ginSwagger.WrapHandler(
+			swaggerfiles.Handler,
+			//ginSwagger.URL(fmt.Sprintf("http://localhost:%d/swagger/doc.json", conf.GetInt("app.http.port"))),
+			ginSwagger.DefaultModelsExpandDepth(-1),
+		))
 	}
-	s.Static("/css", "web/static/css")
-	// 加载模板引擎，并指定自定义函数
-	s.SetHTMLTemplate(template.Must(template.New("").Funcs(funcMap).ParseGlob("web/templates/**/*")))
-
-	// swagger doc
-	docs.SwaggerInfo.BasePath = "/v1"
-	s.GET("/swagger/*any", ginSwagger.WrapHandler(
-		swaggerfiles.Handler,
-		//ginSwagger.URL(fmt.Sprintf("http://localhost:%d/swagger/doc.json", conf.GetInt("app.http.port"))),
-		ginSwagger.DefaultModelsExpandDepth(-1),
-	))
 
 	s.Use(
 		middleware.CORSMiddleware(),
